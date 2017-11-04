7f6488bab08e4b1f19687a859146741651d2014e
msppict <msppictentc@gmail.com>
2017-11-04 16:40:55 +0530

    Finding the max occuring number

--- project1_C.c
+++ project1_C.c
@@ -4,15 +4,37 @@ void main(){
 	printf("Hello World!! \n");
 
 	int a[] = {1,23,3,1,3,1,2,1,1,33,1,1,4,1,34,1,1,4,1,5,1,1,5,1,5,1,6,1,1};
-	int size = sizeof a[];
-	printf("Sizes of a = %d",&size);
-	printf("Array is given as \n");
+	printf("Array is given as \n {");
+	int size = sizeof a/sizeof (int);
 	int i = 0;
-	for(i=0;i<size;i++){
+	for(i=0;i < size;i++){
 
-		printf("%d",a[i]);
+		printf("%d  ",a[i]);
 
 	}
+	printf("} \n");
+
+	int g = a[0];
+	int temp = 0;
+	int count = 0;
+	for(i=0;i< size;i++){
+
+		temp = a[i];
+		if(g==temp){
+			count++;
+		}
+		else{
+			if(count == 0){
+				g = temp;
+				count = 0;
+			}
+			else{
+				count--;
+			}
+		}
+	}
+
+	printf("\n The number with max occurance = %d", g);
 
 
 }
